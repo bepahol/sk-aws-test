e33e0e8cce13066964c4ad0ac401a8dccc247154
Benjamin Holst <bepahol2@gmail.com>
2018-05-24 16:07:24 -0600

    added full path to java

--- runInstances.sh
+++ runInstances.sh
@@ -8,5 +8,5 @@ fi
 file_path=com/ms/silverking/aws/MultiInstanceLauncher
 
 cd src/
-javac -cp .:../lib/aws-java-sdk-1.11.333/lib/* $file_path.java
-java  -cp .:../lib/aws-java-sdk-1.11.333/lib/*:../lib/aws-java-sdk-1.11.333/third-party/lib/* $file_path $1
+/usr/lib/jvm/java-1.8.0/bin/javac -cp .:../lib/aws-java-sdk-1.11.333/lib/* $file_path.java
+/usr/lib/jvm/java-1.8.0/bin/java  -cp .:../lib/aws-java-sdk-1.11.333/lib/*:../lib/aws-java-sdk-1.11.333/third-party/lib/* $file_path $1
