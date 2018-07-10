32ee135e30103f945b548de84939ff80f8fc17ad
Benjamin Holst <bepahol2@gmail.com>
2018-07-09 22:10:48 -0600

    commenting out quotes

--- spark_compile_app.sh
+++ spark_compile_app.sh
@@ -32,14 +32,14 @@ import org.apache.spark.sql.Dataset;
 
 public class SimpleApp {
   public static void main(String[] args) {
-    String logFile = "README.md"; // Should be some file on your system
-    SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+    String logFile = \"README.md\"; // Should be some file on your system
+    SparkSession spark = SparkSession.builder().appName(\"Simple Application\").getOrCreate();
     Dataset<String> logData = spark.read().textFile(logFile).cache();
 
-    long numAs = logData.filter(s -> s.contains("a")).count();
-    long numBs = logData.filter(s -> s.contains("b")).count();
+    long numAs = logData.filter(s -> s.contains(\"a\")).count();
+    long numBs = logData.filter(s -> s.contains(\"b\")).count();
 
-    System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+    System.out.println(\"Lines with a: \" + numAs + \", lines with b: \" + numBs);
 
     spark.stop();
   }
@@ -53,14 +53,14 @@ import org.apache.spark.sql.Dataset;
 
 public class SimpleApp {
   public static void main(String[] args) {
-    String logFile = "/var/tmp/silverking/skfs/skfs_mnt/skfs/README.md"; // Should be some file on your system
-    SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+    String logFile = \"/var/tmp/silverking/skfs/skfs_mnt/skfs/README.md\"; // Should be some file on your system
+    SparkSession spark = SparkSession.builder().appName(\"Simple Application\").getOrCreate();
     Dataset<String> logData = spark.read().textFile(logFile).cache();
 
-    long numAs = logData.filter(s -> s.contains("a")).count();
-    long numBs = logData.filter(s -> s.contains("b")).count();
+    long numAs = logData.filter(s -> s.contains(\"a\")).count();
+    long numBs = logData.filter(s -> s.contains(\"b\")).count();
 
-    System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+    System.out.println(\"Lines with a: \" + numAs + \", lines with b: \" + numBs);
 
     spark.stop();
   }
